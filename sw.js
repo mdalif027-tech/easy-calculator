6abf0e39fae88d90fc58c26e4cc4256af6456ca6
mdalif027-tech <mdalif027@gmail.com>
2026-08-28 20:27:22 +0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,39 @@
-const CACHE_NAME = "easy-calculator-v1";
-const urlsToCache = [
-  "./index.html",
-  "./style.css",
-  "./script.js"
+// Change cache name to force browsers to throw away the old files
+const CACHE_NAME = 'easy-calc-v3'; 
+
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Force activation immediately
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // Clear out the old version
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
